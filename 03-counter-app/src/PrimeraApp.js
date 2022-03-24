3fbbf2a27f24742b11a09249a8c459d95502e380
Daniel Alexander Muelas Rivera <desarrollador@seguridadomega.com.co>
2022-03-23 19:30:21 -0500

    Primera app en react

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PrimeraApp = () => {
+    return (
+        <>
+            <h1> Hola Mundo </h1>
+            <p> Mi primera app en react </p>
+        </>
+    );
+};
+
+export default PrimeraApp;
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,1 +1,9 @@
-console.log('Hola Imundo')
+import React from "react";
+import ReactDOM from "react-dom";
+import PrimeraApp from "./PrimeraApp";
+
+import "./index.css"
+
+const divRoot = document.querySelector('#root');
+
+ReactDOM.render(<PrimeraApp />, divRoot);
